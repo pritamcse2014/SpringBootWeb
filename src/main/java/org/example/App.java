6fa63f4e0dc3de6db306a8402799af6c12b03acb
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-07-06 22:57:29 +0600

    RespondingToTheClient

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,21 +4,24 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args ) throws LifecycleException {
+    public static void main( String[] args ) throws LifecycleException, IOException {
         System.out.println( "Hello World!" );
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        tomcat.setPort(9090);
+        tomcat.getConnector();
 
-        Context context = tomcat.addContext("", null);
+        Context context = tomcat.addContext("", new File(".").getCanonicalPath());
         Tomcat.addServlet(context, "HelloServlet", new HelloServlet());
         context.addServletMappingDecoded("/hello", "HelloServlet");
-
         tomcat.start();
         tomcat.getServer().await();
     }
--- src/main/java/org/example/HelloServlet.java
+++ src/main/java/org/example/HelloServlet.java
@@ -4,8 +4,16 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class HelloServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse res) {
-        System.out.println("Hello World!");
+//    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        System.out.println("Hello World!");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2><b>Hello World!</b></h2>");
+//        res.getWriter().println("Hello World!");
     }
 }
